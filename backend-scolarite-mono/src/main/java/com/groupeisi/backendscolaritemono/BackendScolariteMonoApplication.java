26bb78b00b2e4b6fa5f719b2755552886876eed5
che444 <macdialloisidk@groupeisi.com>
2024-01-12 12:09:09 +0000

    Feat/SMOPAI-188-S1-start-project

---
+++ backend-scolarite-mono/src/main/java/com/groupeisi/backendscolaritemono/BackendScolariteMonoApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.backendscolaritemono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendScolariteMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendScolariteMonoApplication.class, args);
+	}
+
+}
---
+++ backend-scolarite-mono/src/test/java/com/groupeisi/backendscolaritemono/BackendScolariteMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.backendscolaritemono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendScolariteMonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
